572c8c95e972da6eaad08dd17d4d127c3b36aafa
alarivan <alarivan@pm.me>
2019-06-05 15:07:08 -0700

    Add landing block to login page

---
+++ src/components/Landing.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container mx-auto">
+    <div class="flex flex-wrap mb-20">
+      <div
+        v-for="(item, index) in items"
+        :key="index"
+        class="w-full sm:w-1/2 p-4 pb-10 text-center"
+      >
+        <h3 class="text-xl font-bold mb-2">{{item.title}}</h3>
+        <p class="max-w-xs mx-auto text-lg" v-html="item.description"></p>
+      </div>
+    </div>
+    <div class="w-full mx-auto py-2 mb-8 text-center">
+      <h4>contact developer:</h4>
+      <a class="underline" href="mailto:alarivan@pm.me">alarivan@pm.me</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "landing",
+  data() {
+    return {
+      items: [
+        {
+          title: "Encrypted",
+          description:
+            "Your data is encrypted with keys that only you hold via <a class='underline hover:text-green-600' href='https://blockstack.org' target='_blank'>Blockstack.</a>"
+        },
+        {
+          title: "Quick and Simple",
+          description:
+            "Find, edit and copy your notes in seconds using advanced search and tags."
+        },
+        {
+          title: "Decentralised",
+          description: "You are in control of where your data is stored."
+        },
+        {
+          title: "Access anywhere",
+          description:
+            "You have acces to your notes on mobile and desktop devices."
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,29 +1,34 @@
 <template>
-  <div class="flex justify-center items-center h-70vh">
-    <div class="text-center sm:w-1/2">
-      <h1 class="mb-4 text-2xl font-bold">BlockNote.xyz</h1>
-      <p class>Note taking application with keyboard shortcuts for everything.</p>
-      <p class="mb-3">
-        Built with
-        <a
-          class="underline hover:text-green-600"
-          href="https://blockstack.org"
-          target="_blank"
-        >Blockstack</a>
-      </p>
-      <button
-        class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4"
-        @click.prevent="signIn"
-      >Sign In With Blockstack</button>
+  <div>
+    <div class="flex justify-center items-center h-70vh">
+      <div class="text-center sm:w-1/2">
+        <h1 class="mb-4 text-2xl font-bold">BlockNote.xyz</h1>
+        <p class>Note taking application with keyboard shortcuts for everything.</p>
+        <p class="mb-3">
+          Built with
+          <a
+            class="underline hover:text-green-600"
+            href="https://blockstack.org"
+            target="_blank"
+          >Blockstack</a>
+        </p>
+        <button
+          class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4"
+          @click.prevent="signIn"
+        >Sign In With Blockstack</button>
+      </div>
     </div>
+    <landing/>
   </div>
 </template>
 
 <script>
 import { userSession } from "../helper/userSession";
+import Landing from "../components/Landing";
 
 export default {
   name: "login",
+  components: { Landing },
   methods: {
     signIn() {
       userSession.redirectToSignIn();
